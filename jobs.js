8bd9a35631bf1c5b55e8cc29fa847c317f1db69f
Sergey Ovechkin <me@pomeo.me>
2015-08-05 08:53:31 +0300

    increase time

--- jobs.js
+++ jobs.js
@@ -77,7 +77,7 @@ setInterval(function() {
       }).priority('normal').removeOnComplete(true).save();
     }
   });
-}, 1000 );
+}, 50000 );
 
 jobs.process('emailRegister', function(job, done) {
   var domain = require('domain').create();
