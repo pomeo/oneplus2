39e36d70244ff44a222ee1529a81014934f6bd9e
Sergey Ovechkin <me@pomeo.me>
2015-09-11 11:10:26 +0300

    increase timeout

--- jobs.js
+++ jobs.js
@@ -148,13 +148,13 @@ jobs.process('clickConfirm', function(job, done) {
     rest.get(job.data.url, {
       //headers: {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'},
       headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.132 Safari/537.36'},
-      timeout: 2000
+      timeout: 20000
     }).once('timeout', function(ms){
       log('Ошибка: Таймаут ' + ms + ' ms', 'error');
-      // jobs.create('clickConfirm', {
-      //   to: job.data.to,
-      //   url: job.data.url
-      // }).delay(60000).priority('low').removeOnComplete(true).save();
+      jobs.create('clickConfirm', {
+        to: job.data.to,
+        url: job.data.url
+      }).delay(60000).priority('low').removeOnComplete(true).save();
       setImmediate(done);
     }).once('error',function(err, response) {
       log('Ошибка: ' + err, 'error');
@@ -166,7 +166,6 @@ jobs.process('clickConfirm', function(job, done) {
       log('Ошибка: ' + JSON.stringify(data), 'error');
       setImmediate(done);
     }).once('success',function(data, response) {
-      log(response);
       log('Подтверждена почта ' + job.data.mail);
       setImmediate(done);
     });
