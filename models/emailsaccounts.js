f238cd8404b9a7c6176a5df84661fad04cc03aa3
Sergey Ovechkin <me@pomeo.me>
2015-09-17 13:22:31 +0300

    change model accounts with emails

--- models/emailsaccounts.js
+++ models/emailsaccounts.js
@@ -4,12 +4,12 @@ var Schema = mongoose.Schema;
 
 var EmailsAccountsSchema = new Schema({
   email       : { type: String, unique: true }, // почтовый адрес
-  hash        : { type: String, index: true }, // хеш
+  urlhash     : { type: String, index: true }, // хеш
   password    : String, // пароль от аккаунта
   invite      : { type: Boolean, index: true }, // инвайт в аккаунте
   sell        : { type: Boolean, index: true }, // продан аккаунт
-  start       : { type: Date, index: true }, // дата начала инвайта
-  end         : { type: Date, index: true }, // дата конца инвайта
+  start       : { type: Number, index: true }, // дата начала инвайта
+  end         : { type: Number, index: true }, // дата конца инвайта
   created_at  : { type: Date }, // дата создания
   updated_at  : { type: Date } // дата изменения
 });
