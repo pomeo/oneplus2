9d803de991459b6f7bae08bcb1f88aba196a04d1
Sergey Ovechkin <me@pomeo.me>
2015-09-21 08:19:20 +0300

    more inform

--- getinvite.rb
+++ getinvite.rb
@@ -102,6 +102,22 @@ def getinvite(url)
           puts m.uri.to_s
           puts m.title
           @a.get('https://invites.oneplus.net/my-invites') do |app|
+            if @a.page.title != 'My invites'
+              urlp = URI.parse('https://api.pushover.net/1/messages.json')
+              req = Net::HTTP::Post.new(urlp.path)
+              req.set_form_data({
+                                  :token => ENV['PUSHOVER_TOKEN'],
+                                  :user => ENV['PUSHOVER_USER'],
+                                  :title => "OnePlus",
+                                  :message => "Error login #{@us.email}"
+                                })
+              res = Net::HTTP.new(urlp.host, urlp.port)
+              res.use_ssl = true
+              res.verify_mode = OpenSSL::SSL::VERIFY_PEER
+              res.start { |http|
+                http.request(req)
+              }
+            end
             inv = Array.new
             app.search('.invite-card').each do |invite|
               if !invite.at('time').nil?
