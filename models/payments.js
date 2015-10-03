dcbf18a7248b42b5d27773f0c4d20d4136077ee0
Sergey Ovechkin <me@pomeo.me>
2015-10-03 16:31:59 +0400

    add payments model

---
+++ models/payments.js
@@ -0,0 +1,16 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PaymentsSchema = new Schema({
+  paymentId   : { type: String, index: true },
+  token       : { type: String, index: true },
+  PayerID     : { type: String, index: true },
+  email       : { type: String, index: true },
+  notes       : String,
+  state       : String,
+  created_at  : { type: Date, default: Date.now }, // дата создания
+  updated_at  : { type: Date, default: Date.now } // дата изменения
+});
+
+mongoose.model('Payments', PaymentsSchema);
