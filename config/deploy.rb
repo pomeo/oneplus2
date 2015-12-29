666e012d30b41f7c5f729aec32a2a64ec3e62a91
Sergey Ovechkin <me@pomeo.me>
2015-12-29 19:10:37 +0300

    add cleanup and symlink folder to capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,13 +10,21 @@ offroad_modules   "defaults", "supervisord"
 set :repository,  "git@github.com:pomeo/oneplus2.git"
 set :supervisord_start_group, "oneplus"
 set :supervisord_stop_group,  "oneplus"
-set :shared_children, shared_children + %w{tmp}
 #========================
 #ROLES
 #========================
 set  :gateway,    "#{application}" # main server
-role :app,        "10.3.140.1"      # container
+role :app,        "10.3.140.1"     # container
+
+namespace :deploy do
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -s #{shared_path}/tmp #{release_path}/tmp"
+  end
+end
 
 after "deploy:create_symlink",
       "deploy:npm_install",
+      "deploy:cleanup",
+      "deploy:symlink_shared",
       "deploy:restart"
